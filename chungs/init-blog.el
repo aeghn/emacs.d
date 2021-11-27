1324d418bda237527bc3bb4de54dff55691beb5c
aeghn <aeghn@outlook.com>
2021-11-27 13:54:58 +0800

    blog

--- chungs/init-blog.el
+++ chungs/init-blog.el
@@ -3,16 +3,39 @@
   :init
   (setq org-static-blog-publish-title "wangz.me"
         org-static-blog-publish-url "https://wangz.me/"
-        org-static-blog-publish-directory "~/Repos/blog/"
+        org-static-blog-publish-directory "~/Repos/blog"
         org-static-blog-posts-directory "~/Repos/blog/posts/"
         org-static-blog-drafts-directory "~/Repos/blog/drafts/"
+        org-static-blog-index-file "index.html"
+        org-static-blog-archive-file "index.html"
+        org-static-blog-langcode "zh"
         org-static-blog-enable-tags t
         org-export-with-toc t
         org-export-with-section-numbers t
         org-static-blog-use-preview t)
 
-  (setq org-static-blog-index-file "archive.html")
-  
+  (defun chin/org-static-blog-insert-date ()
+    (interactive)
+    (goto-char (point-min))
+    (when (and (not (search-forward-regexp "^\\#\\+date:[ ]*<\\([^]>]+\\)>$" nil t))
+               (eq (buffer-local-value 'major-mode (current-buffer)) 'org-mode))
+      (progn
+        (goto-char (point-min))
+        (forward-line)
+        (beginning-of-line)
+        (insert "#+date: ")
+        (org-time-stamp nil)
+        (newline))))
+
+
+  (defun chin/org-static-blog-link-buffer ()
+    (interactive)
+    (let* ((real-file-path (buffer-file-name))
+           (file-name (buffer-name))
+           (symbolic-file-path (expand-file-name file-name org-static-blog-posts-directory)))
+      (message "try to make link: %s -> %s" symbolic-file-path real-file-path)
+      (make-symbolic-link real-file-path symbolic-file-path t)))
+
   (setq org-static-blog-page-header
         "<meta name=\"author\" content=\"Aeghn\">
 <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
